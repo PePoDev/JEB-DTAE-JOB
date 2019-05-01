02c26ab086e9d36bc3f3dfd11df28fd22fc7301e
PePo <gmgame.1525@gmail.com>
2019-05-01 23:13:58 +0700

    Add Assets & Homepage animation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public Image BackgroundFade;
+
+	private Animator animator;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+
+	}
+	public void SwitchPage(int PageIdx)
+	{
+		DOTween.Kill(BackgroundFade);
+		animator.SetInteger("page", PageIdx);
+	}
+	public void FadeBackground(float alpha)
+	{
+		BackgroundFade.DOFade(alpha, 2f);
+	}
+}
---
+++ Assets/Scripts/LoadingTween.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingTween : MonoBehaviour
+{
+	public Vector2 TargetPosotion;
+	public float Duration;
+	public bool StartOnAwkae;
+
+	private void Start()
+	{
+		if (StartOnAwkae)
+		{
+			GetComponent<RectTransform>().DOAnchorPos(TargetPosotion, Duration);
+			StartCoroutine(WaitTween());
+		}
+		IEnumerator WaitTween()
+		{
+			yield return new WaitForSeconds(Duration + 0.5f);
+			Initiate.Fade(SceneManager.GetActiveScene().buildIndex + 1, Color.black, Duration);
+		} 
+	}
+}
