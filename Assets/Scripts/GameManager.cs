082e2de5ad1412004fdbeca5ff1e5040919b1b2d
PePo <gmgame.1525@gmail.com>
2019-05-02 04:35:06 +0700

    2 Phase layout complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,39 @@
 ﻿using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-	public Image BackgroundFade;
+	public Image ImageBackgroundFade;
+	public Image ImagePopup;
+	public Sprite[] popupSprite;
+	public Image ImageRecheck;
+	public Sprite[] recheckSprite;
+	public Image ImageHead;
+	public Sprite[] headSprite;
+	public Animator phase2_Animator;
 
 	private Animator animator;
 	private int currentPage;
+	private int currentPhase;
+	private int currentPopup;
+
+	public object IEnumberator { get; private set; }
 
 	private void Start()
 	{
 		animator = GetComponent<Animator>();
 		currentPage = 1;
+		currentPhase = 4;
+		currentPopup = 0;
 	}
 
-	private void Update()
+	public void NextPage()
 	{
-
+		currentPhase++;
+		ChangeRecheck(currentPhase - 4);
+		SwitchPage(currentPhase);
 	}
 	public void SwitchPage(int PageIdx)
 	{
@@ -26,19 +42,55 @@ public class GameManager : MonoBehaviour
 			animator.SetInteger("page", PageIdx);
 			currentPage = PageIdx;
 
-			var alpha = 0f;
-			DOTween.Kill(BackgroundFade);
+			var alpha = 1f;
+			DOTween.Kill(ImageBackgroundFade);
 			switch (PageIdx)
 			{
 				case 1:
 					alpha = 0f;
 					break;
-				case 2:
-				case 3:
-					alpha = 1f;
-					break;
 			}
-			BackgroundFade.DOFade(alpha, 2f);
+			ImageBackgroundFade.DOFade(alpha, 2f);
 		}
+	}
+	public void ColorAlphaFadeIn(Image image)
+	{
+		image.DOFade(endValue: 1f, duration: 1f);
+	}
+	public void ColorAlphaFadeOut(Image image)
+	{
+		image.DOFade(endValue: 0f, duration: 1f);
+	}
+	public void ChangePopup(int i)
+	{
+		if (currentPopup + i < 0)
+		{
+			return;
+		}
+
+		if (currentPopup + i == popupSprite.Length)
+		{
+			ColorAlphaFadeOut(ImagePopup);
+			animator.SetInteger("page", 10);
+			StartCoroutine(WaitAnimation());
+			return;
+		}
+
+		currentPopup += i;
+		ImagePopup.sprite = popupSprite[currentPopup];
+
+		IEnumerator WaitAnimation()
+		{
+			yield return new WaitForSeconds(2f);
+			ImagePopup.gameObject.SetActive(false);
+		}
+	}
+	public void ChangeRecheck(int i)
+	{
+		ImageRecheck.overrideSprite = recheckSprite[i];
+	}
+	public void PlayPhase2()
+	{
+		phase2_Animator.SetTrigger("new");
 	}
 }
